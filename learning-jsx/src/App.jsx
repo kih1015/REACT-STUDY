d52b02710c7b407d516ba604c45f0c5264a81dad
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 02:23:09 +0900

    feat: 중괄호 표현식2

--- learning-jsx/src/App.jsx
+++ learning-jsx/src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
 
-const language = "JavaScript";
+function BasicExpressions() {
+  const name = "John";
+  const age = 25;
+  const isAdmin = true;
+
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <p>Age next year: {age + 1}</p>
+      <p>{name + "'s Profile"}</p>
+      <p>{`${name} is ${age} years old`}</p>
+      <p>Admin status: {String(isAdmin)}</p>
+    </div>
+  );
+}
 
 function App() {
-  return <>{language}</>;
+  return <BasicExpressions />;
 }
 
 export default App;
