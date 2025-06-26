2e693cdf74c12fab2e9b9fcc56bdd424d24586f2
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 04:49:38 +0900

    feat: 함수 전달

--- learning-state/src/App.jsx
+++ learning-state/src/App.jsx
@@ -2,14 +2,18 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   const [isPinned, setPinned] = useState(false);
 
   const togglePinned = () => {
-    setPinned(!isPinned);
+    setPinned((p) => !p);
   };
 
   return (
     <>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <button onClick={() => setCount((c) => c - 1)}>-</button>
       <button onClick={togglePinned}>{isPinned && "📌"} Pinn This!</button>
     </>
   );
