b4b3b95cf00aae80daa09adf911e0b96177900d4
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 07:29:43 +0900

    feat: useEffect 사용 예제

--- learning-hooks/src/App.jsx
+++ learning-hooks/src/App.jsx
@@ -1,26 +1,27 @@
 import "./App.css";
-import ClassComp from "./ClassComp";
-import FuncComp from "./FuncComp";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  const [selected, setSelected] = useState("");
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const handleIncrease = (setter) => {
+    setter((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    console.log(`C1: ${count1}, C2: ${count2}`);
+  }, [count1]);
 
   return (
-    <>
-      {["", "ClassComp", "FuncComp"].map((option) => (
-        <label key={option}>
-          <input
-            type="radio"
-            value={option}
-            checked={selected === option}
-            onChange={(e) => setSelected(e.target.value)}
-          />
-          {option || "None"}
-        </label>
-      ))}
-      {selected && (selected === "ClassComp" ? <ClassComp /> : <FuncComp />)}
-    </>
+    <div>
+      <h2>Count1: {count1}</h2>
+      <button onClick={() => handleIncrease(setCount1)}>Count1++</button>
+
+      <h2>Count2: {count2}</h2>
+      <button onClick={() => handleIncrease(setCount2)}>Count2++</button>
+    </div>
   );
 };
+
 export default App;
