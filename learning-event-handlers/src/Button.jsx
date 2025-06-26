6c70c1d4c895178e8a826f923dd0b87fd5dfc071
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 04:27:39 +0900

    feat: 이벤트 객체

--- learning-event-handlers/src/Button.jsx
+++ learning-event-handlers/src/Button.jsx
@@ -1,5 +1,11 @@
+const handleEvent = (name, e) => {
+  console.log(name, e);
+  console.log(name, e.clientX, e.clientY);
+  console.log(name, e.shiftKey);
+};
+
 const Button = ({ name }) => (
-  <button onClick={() => console.log(name)}>{name}</button>
+  <button onClick={(e) => handleEvent(name, e)}>{name}</button>
 );
 
 export default Button;
