f2e56755ea7f518c396dcfe95d8596137ec56d1c
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 04:59:47 +0900

    feat: 배열 스테이트

--- learning-state/src/App.jsx
+++ learning-state/src/App.jsx
@@ -2,19 +2,36 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [isPinned, setPinned] = useState(false);
+  const [todos, setTodos] = useState(["Learn React", "Build a project"]);
 
-  const togglePinned = () => {
-    setPinned((p) => !p);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  };
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
   };
 
   return (
     <>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount((c) => c + 1)}>+</button>
-      <button onClick={() => setCount((c) => c - 1)}>-</button>
-      <button onClick={togglePinned}>{isPinned && "📌"} Pinn This!</button>
+      <h3>Todo List</h3>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo || "비어 있는 값"}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <p>Typing: {newTodo}</p>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={() => addTodo(newTodo)}>Add New Task</button>
     </>
   );
 }
