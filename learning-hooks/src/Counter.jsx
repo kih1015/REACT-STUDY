400db7563b84ee3d3c157fe8739e362a6a75d54c
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 06:35:03 +0900

    feat: useRef + useState

--- learning-hooks/src/Counter.jsx
+++ learning-hooks/src/Counter.jsx
@@ -1,21 +1,27 @@
 import { useState, useRef } from "react";
 
 function Counter() {
-  const count1 = useRef(0);
+  const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
+  const refCount = useRef(0);
 
   const incrementRef = () => {
-    count1.current += 1;
-    console.log("Ref Count:", count1.current);
+    refCount.current += 1;
+    console.log("Ref Count:", refCount.current);
+  };
+
+  const syncCounts = () => {
+    setCount1(refCount.current);
+    setCount2((prev) => prev + 1);
   };
 
   return (
     <>
       <h2>Counter Counter</h2>
-      <p>Count 1: {count1.current}</p>
+      <p>Count 1: {count1}</p>
       <p>Count 2: {count2}</p>
       <button onClick={incrementRef}>useRef</button>
-      <button onClick={() => setCount2((c) => c + 1)}>useState</button>
+      <button onClick={syncCounts}>useState</button>
     </>
   );
 }
