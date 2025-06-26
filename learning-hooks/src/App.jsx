f0ad03a2335dbce72af8973d6f90423daeb35b8d
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 08:52:38 +0900

    feat: 최적화 전

--- learning-hooks/src/App.jsx
+++ learning-hooks/src/App.jsx
@@ -1,27 +1,29 @@
 import "./App.css";
-import { useFetch } from "./hooks/useFetch";
+import { useState } from "react";
 
-const App = () => {
-  const { data, loading, error } = useFetch("http://localhost:3001/books");
+function App() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const heavyCalculation = (num) => {
+    console.log("Calculating...");
+    let result = 0;
+    for (let i = 0; i < 1000000000; i++) {
+      result += num;
+    }
+    return result;
+  };
 
   return (
     <>
-      <h2>Book List</h2>
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>Error: {error}</p>
-      ) : (
-        <ul>
-          {data.map((book) => (
-            <li key={book.id}>
-              <strong>{book.title} </strong>
-              by {book.author}
-            </li>
-          ))}
-        </ul>
-      )}
+      <p>Counter 1: {count1}</p>
+      <button onClick={() => setCount1(count1 + 1)}>+</button>
+      <button onClick={() => setCount1(count1 - 1)}>-</button>
+      <p>Counter 2: {heavyCalculation(count2)}</p>
+      <button onClick={() => setCount2(count2 + 1)}>+</button>
+      <button onClick={() => setCount2(count2 - 1)}>-</button>
     </>
   );
-};
+}
+
 export default App;
