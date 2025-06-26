9ceeb30a378a6331abe3b30b25752211c9dc2754
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 02:21:10 +0900

    feat: 중괄호 표현식

--- learning-jsx/src/App.jsx
+++ learning-jsx/src/App.jsx
@@ -1,27 +1,9 @@
 import "./App.css";
 
+const language = "JavaScript";
+
 function App() {
-  const element1 = <h2>Hello, world!</h2>;
-  const element2 = (
-    <ul>
-      <li>A</li>
-      <li>B</li>
-      <li>C</li>
-    </ul>
-  );
-
-  console.log(element1);
-  console.log(element2);
-
-  return (
-    <>
-      {/* JSX Practices */}
-      <h1>JSX</h1>
-
-      {element1}
-      {element2}
-    </>
-  );
+  return <>{language}</>;
 }
 
 export default App;
