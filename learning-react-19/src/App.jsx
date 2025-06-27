ef9cf875dc4312f32e0a316ba0e06f481b60c369
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 10:33:58 +0900

    feat: 메타 정보

--- learning-react-19/src/App.jsx
+++ learning-react-19/src/App.jsx
@@ -1,28 +1,18 @@
 import "./App.css";
-import { useState, useOptimistic } from "react";
-import { updateName } from "./asynctasks";
 
-const App = () => {
-  const [name, setName] = useState("(Empty)");
-  const [optName, setOptName] = useOptimistic(name);
-
-  const submitAction = async (formData) => {
-    const newName = formData.get("name");
-    setOptName(`⏳ ${newName}`);
-    const updatedName = await updateName(newName);
-    setName(updatedName);
+function App() {
+  const post = {
+    title: "About React 19",
+    author: "Yalco",
   };
 
   return (
-    <form action={submitAction}>
-      <p>Your name is: {optName}</p>
-      <p>
-        <label>Change Name:</label>
-        <input type="text" name="name" />
-        <button type="submit">Update</button>
-      </p>
-    </form>
+    <>
+      <h1>{post.title}</h1>
+      <title>{post.title}</title>
+      <meta name="author" content={post.author} />
+    </>
   );
-};
+}
 
 export default App;
