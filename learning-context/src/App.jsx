3d6a7eb67812a4db01bd7832cda5c867c5b8b261
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 09:51:48 +0900

    feat: useNavigate 사용

--- learning-context/src/App.jsx
+++ learning-context/src/App.jsx
@@ -1,18 +1,21 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 function App() {
+  const navigate = useNavigate();
+
+  const navTo = (path) => {
+    navigate(path);
+  };
+
   return (
     <>
-      <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/contact"}>Contact</Link>
-      </nav>
+      <button onClick={() => navTo("/")}>Home</button>
+      <button onClick={() => navTo("/about")}>About</button>
+      <button onClick={() => navTo("/contact")}>Contact</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
