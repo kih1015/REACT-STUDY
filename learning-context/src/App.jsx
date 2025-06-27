84a6efc3492fe2da9327b46f346c2784061bbfde
강인화 <rkddlsghk1998@gmail.com>
2025-06-27 09:39:37 +0900

    feat: Context 추가

--- learning-context/src/App.jsx
+++ learning-context/src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
 import { CountProvider } from "./contexts/CountContext";
+import { ToggleProvider } from "./contexts/ToggleContext";
 import Child1 from "./Child1";
+import Child3 from "./Child3";
 
 function App() {
   return (
-    <CountProvider>
+    <>
       <h2>App</h2>
-      <Child1 />
-    </CountProvider>
+      <CountProvider>
+        <Child1 />
+      </CountProvider>
+      <ToggleProvider>
+        <Child3 />
+      </ToggleProvider>
+    </>
   );
 }
 
